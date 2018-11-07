59ee3a54b5fda0e6002febaca9967309f19b6d7f
BarnabyRW <barnaby.rw@gmail.com>
2018-11-07 14:31:40 +1100

    ok

---
+++ Assets/Scripts/DisableSelf.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableSelf : MonoBehaviour {
+    public float lifeTime = 6f;
+
+	void OnEnable()
+    {
+        StartCoroutine(Disable());
+    }
+
+    IEnumerator Disable()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        this.gameObject.SetActive(false);
+    }
+}
